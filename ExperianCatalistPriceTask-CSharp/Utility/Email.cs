7f1b93157f8f5cb23a2b49ca5c2194585a7b1192
SureShotUK <irwin455@hotmail.com>
2023-03-21 17:17:51 +0000

    Removed Microsoft.Exhange.WebServices nuget package

--- ExperianCatalistPriceTask-CSharp/Utility/Email.cs
+++ ExperianCatalistPriceTask-CSharp/Utility/Email.cs
@@ -18,67 +18,6 @@ namespace ExperianCatalistPriceTask_CSharp.Utility
 {
     public class GetEmailAttachment
     {
-        //public static bool DownloadEmailSpreadSheet()
-        //{
-        //    EmailReceiverCreds creds = Credentials.GetEmailReceiverCreds();
-        //    // Set up the ExchangeService object with your credentials and the URL of the EWS endpoint
-        //    ExchangeService service = new()
-        //    {
-        //        Credentials = new WebCredentials(creds.EmailReceiverUserName, creds.EmailReceiverPassword),
-        //        Url = new Uri("https://outlook.office365.com/EWS/Exchange.asmx")
-        //    };
-
-        //    // Define the email subject to search for
-        //    string emailSubject = "Experian Catalist Price Averages";
-        //    // We want to find an Email that has been sent today - so it must be greater than the last minute/second of YESTERDAY to qualify as being TODAY. 
-        //    DateTime yesterday = DateTime.Now.AddDays(-1);
-        //    TimeSpan ts = new(23, 59, 59);
-        //    yesterday = yesterday.Date + ts;
-
-        //    // Construct the search filter to find emails with the specified subject
-        //    SearchFilter.SearchFilterCollection searchFilterCollection = new(LogicalOperator.And)
-        //    {
-        //        new SearchFilter.ContainsSubstring(ItemSchema.Subject, emailSubject),
-        //        new SearchFilter.IsGreaterThan(ItemSchema.DateTimeReceived, yesterday)
-        //    };
-
-        //    // Set up the ItemView object to retrieve only the email messages that match the search filter
-        //    ItemView view = new(1)
-        //    {
-        //        PropertySet = new PropertySet(BasePropertySet.IdOnly)
-        //    };
-        //    view.OrderBy.Add(ItemSchema.DateTimeReceived, SortDirection.Descending);
-
-        //    // Use the FindItems method to search for emails that match the search filter
-        //    FindItemsResults<Item> findResults = service.FindItems(WellKnownFolderName.Inbox, searchFilterCollection, view);
-
-        //    // Loop through the messages and download any attachments
-        //    foreach (Item item in findResults.Items)
-        //    {
-        //        // Bind the item to a new EmailMessage object so that we can access its properties and attachments
-        //        EmailMessage message = EmailMessage.Bind(service, item.Id);
-
-        //        foreach (Microsoft.Exchange.WebServices.Data.Attachment attachment in message.Attachments)
-        //        {
-        //            // Check if the attachment is a file attachment
-        //            if (attachment is FileAttachment)
-        //            {
-        //                FileAttachment fileAttachment = attachment as FileAttachment;
-        //                string attachmentPath = Path.Combine(Directory.GetCurrentDirectory(), "Experian Catalist Price Averages.xlsx");
-
-        //                // Download the attachment
-        //                fileAttachment.Load(attachmentPath);
-        //                return true;
-        //            }
-        //        }
-        //    }
-        //    StringBuilderPlusConsole.ErrorEmailBodyBuilderSBOnly("<p>Target Destination: Portland > Prices >  <b>Pump Prices vs Platts.xlsx</b></p> <hr>");
-        //    StringBuilderPlusConsole.ErrorEmailBodyBuilder("No E-mail from Experian for today's date can be found in the inbox of prices@portland-fuel.co.uk.");
-        //    StringBuilderPlusConsole.ErrorEmailBodyBuilder("This is perfectly normal and happens occasionally as the Experian E-mails do not come in daily.");
-        //    StringBuilderPlusConsole.ErrorEmailBodyBuilder("It is recommended to check the inbox of prices@portland-fuel.co.uk, to see if an E-mail <i>was</i> received, in which case this program is having difficulties downloading said E-mail.");
-        //    return false;
-        //}
-
         public static async Task<bool> DownloadExperianCatalistFileAsync()
         {
             EmailReceiverCreds creds = Credentials.GetEmailReceiverCreds();
